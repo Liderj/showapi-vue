efe00b0ef4dba954e92b088533a2e16f59e1e8f9
lider <liwanjun@honc.tech>
2017-03-10 18:42:26 +0800

    reset.css

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 
+const Home = resolve => require(['@/components/Home'], resolve)
+const Demo = resolve => require(['@/components/Demo'], resolve)
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/d',
+      name: 'Dome',
+      component: Demo
     }
   ]
 })
